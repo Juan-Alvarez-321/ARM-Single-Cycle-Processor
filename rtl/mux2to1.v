84ec2e6a25e7d9f9e5c60a98d2d8497949f81002
Juan Alvarez <jalvarezduque@hotmail.com>
2026-08-21 15:11:40 -0500

    Finalized 2 to 1 mux module

--- rtl/mux2to1.v
+++ rtl/mux2to1.v
@@ -1,15 +1,22 @@
 `timescale 1ns / 1ps
 
-//---------------------------------------------
-// mux2to1
-// select = 0 -> ouptut = in0
-// select = 1 -> output = in1
-//---------------------------------------------
+//==============================================================================
+// Module      : mux2to1
+// Description : Parametrized 2 to 1 multiplexer. Only combinational
+//               select = 0 -> output = in0
+//               select = 1 -> output = in1
+//
+// Parameters  : WIDTH  - mux input bit width (default 64)
+//
+//
+// Author      : Juan Alvarez
+// Date        : 2026-07-13
+//==============================================================================
 
 
 module mux2to1 #(
     parameter WIDTH = 64
-)()
+)(
     input  wire [WIDTH-1:0] in0,   // Input 0
     input  wire [WIDTH-1:0] in1,   // Input 1
     input  wire       sel,         // Select signal
