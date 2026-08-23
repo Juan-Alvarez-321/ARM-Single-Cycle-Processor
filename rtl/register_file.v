c136b6c6bb0b2e8d96f91f7ed95b463718d493bf
Juan Alvarez <jalvarezduque@hotmail.com>
2026-08-23 18:53:13 -0500

    Finalized Register File

--- rtl/register_file.v
+++ rtl/register_file.v
@@ -2,7 +2,8 @@
 
 //==============================================================================
 // Module      : register_file
-// Description : 
+// Description : 32 x 64 register file with read (2 ports) and write (1 port)
+//               Zero register can only read 0  
 //
 // Parameters  : WIDTH        - register data width (default 64)
 //               ADDR_WIDTH   - register address width (default 5)          
@@ -16,8 +17,8 @@
 
 module RegisterFile #(
      parameter WIDTH = 64,
-     parameter ADDR_WIDTH = 5,
-     parameter DEPTH = 32
+     parameter ADDR_WIDTH = 5
+
 )(
 
 
@@ -29,29 +30,39 @@ module RegisterFile #(
      input  wire [WIDTH-1:0]       bus_w,  // Data stored in register
 
 
-     output wire [WIDTH-1:0]       bus_a,  // Register A data
-     output wire [WIDTH-1:0]       bus_b  // Register B data
+     output reg [WIDTH-1:0]       bus_a,  // Register A data
+     output reg [WIDTH-1:0]       bus_b  // Register B data
 
 );
+
+localparam [ADDR_WIDTH-1:0] ZR = 31;
+localparam DEPTH = 1 << ADDR_WIDTH;
+
 
 
 reg [WIDTH-1:0] regs [DEPTH-1:0];
 
 initial begin
-     for(intger i = 0; i < DEPTH; i = i + 1) begin
-          regs[i] = {WIDTH}'b0;
+     for(integer i = 0; i < DEPTH; i = i + 1) begin
+          regs[i] = 0;
 
      end
 end
 
-initial begin
+
+always @(*) begin
+
+     bus_a = (ra == ZR) ? {WIDTH{1'b0}} : regs[ra];
+
+     bus_b = (rb == ZR) ? {WIDTH{1'b0}} : regs[rb];
+
+     
+end
+
+always @(posedge clk) begin
+     if(reg_wr && rw != ZR)
+          regs[rw] <= bus_w;
+end
 
 
-
-
-
-
-
-
-
-	
+endmodule
